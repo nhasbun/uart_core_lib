ec46fdfdd0f1a83c3ec595aa13dc4b8d6b3862fe
Nicolas Hasbun <nhasbun@gmail.com>
2017-07-28 00:01:06 -0400

    Activacion de buffer para datos entrantes usando pthreads

--- uart_core_lib.c
+++ uart_core_lib.c
@@ -1,4 +1,5 @@
 #include <stdio.h> // printf setvbuf
+#include <stdlib.h> // free y malloc
 #include <math.h> // round
 #include <string.h> // strlen
 #include "socal/socal.h" // alt_write_hword alt_read_hword
@@ -6,6 +7,8 @@
 #include "uart_core_lib.h"
 
 #define BIT(x,n) (((x) >> (n)) & 1)
+
+// INITIAL CONFIG
 
 UART_T uart_init(uint32_t baseadd)
 {
@@ -18,32 +21,51 @@ UART_T uart_init(uint32_t baseadd)
   uart.DIVISOR   = baseadd + (uint32_t)(0x10);
   uart.ENDOFPACK = baseadd + (uint32_t)(0x14);
 
+  // default buffersize value
+  // 10MB
+  uart.buffer_size = 10000000;
+
   return uart;
 }
-
-uint16_t UartRegister(uint32_t address)
-{
-  return alt_read_hword(address);
-}
-
 
 void setBaud(UART_T * uart_p, uint32_t clock_frequency, uint32_t baudrate)
 {
   // Formula segun
   // Embedded Peripherals IP User Guide -  UART Core
-  float divisor = round((float)clock_frequency/(float)baudrate + 0.5f);
+  float divisor = floor((float)clock_frequency/(float)baudrate + 0.5f);
   alt_write_hword(uart_p->DIVISOR, (uint16_t)divisor);
+}
+
+void setBufferSize(UART_T * uart_p, uint32_t size)
+{
+  uart_p->buffer_size = size;
+  uart_p->buffer_count = 0;
+  uart_p->buffer_pointer_init = malloc(size*sizeof(uint8_t));
+  uart_p->buffer_pointer_writing = uart_p->buffer_pointer_init;
+  uart_p->buffer_pointer_reading = uart_p->buffer_pointer_init;
+}
+
+void freeBuffer(UART_T * uart_p)
+{
+  free(uart_p->buffer_pointer_init);
+}
+
+// UART CORE REGISTER ACCESS
+
+uint16_t uartRegister(uint32_t address)
+{
+  return alt_read_hword(address);
 }
 
 void checkUartRegisters(UART_T * uart_p)
 {
   printf("%s\n", "*******************************");
   printf("%s ", "RX Data:");
-  printf("%i\n", UartRegister(uart_p->RXDATA));
+  printf("%i\n", uartRegister(uart_p->RXDATA));
   printf("%s ", "TX Data:");
-  printf("%i\n", UartRegister(uart_p->TXDATA));
+  printf("%i\n", uartRegister(uart_p->TXDATA));
   printf("%s ", "Status:");
-  uint16_t status = UartRegister(uart_p->STATUS);
+  uint16_t status = uartRegister(uart_p->STATUS);
   printf("%i\n", status);
   printf("%s ", "Exception:");
   printf("%i\n", BIT(status, 8));
@@ -69,38 +91,95 @@ void resetStatus(UART_T * uart_p)
   alt_write_hword(uart_p->STATUS, 0);
 }
 
-// Checkea si hay algun dato en espera a ser leido
-// true si hay un dato en espera
-bool check_rxdata(UART_T * uart_p)
-{
-  uint16_t status = alt_read_hword(uart_p->STATUS);
-  return BIT(status, 7);
-}
+// PROCESO DE ENVIO DE DATOS
 
 // Checkea si el registro esta listo para enviar nuevo caracter
 // true si esta listo
-bool check_txdata(UART_T * uart_p)
+bool checkTxdata(UART_T * uart_p)
 {
   uint16_t status = alt_read_hword(uart_p->STATUS);
   return BIT(status, 6);
 }
 
-void enviarchar(UART_T * uart_p, char character)
+void enviarChar(UART_T * uart_p, char character)
 {
-  while(check_txdata(uart_p) == 0) {}; // txready es 1 cuando
+  while(checkTxdata(uart_p) == 0) {}; // txready es 1 cuando
   // la transmision anterios se completo
   alt_write_hword(uart_p->TXDATA, (uint16_t)character);
 }
 
-void enviarstring(UART_T * uart_p, char * str, int num)
+void enviarString(UART_T * uart_p, char * str, int num)
 {
   for (int i = 0; i < num; ++i) {
-    enviarchar(uart_p, *str);
+    enviarChar(uart_p, *str);
     str++;
   }
 }
 
-char recibirchar(UART_T * uart_p)
+// PROCESO DE RECEPCION DE DATOS
+
+// Checkea si hay algun dato en espera a ser leido
+// true si hay un dato en espera
+bool checkRxdata(UART_T * uart_p)
 {
-  return (char)alt_read_hword(uart_p->RXDATA);
+  uint16_t status = alt_read_hword(uart_p->STATUS);
+  return BIT(status, 7);
+}
+
+uint8_t recibirChar(UART_T * uart_p)
+{
+  return (uint8_t)alt_read_hword(uart_p->RXDATA);
+}
+
+// PROCESO DE RECEPCION AUTOMATICA DE DATOS
+
+void activarRecepcion(UART_T * uart_p)
+{
+  uart_p->thread_run = true;
+  pthread_t thread = uart_p->thread;
+  pthread_create(&thread, NULL, activarRecepcion_aux, uart_p);
+}
+
+void * activarRecepcion_aux(void * ptr)
+{
+  UART_T * uart_p = (UART_T*)ptr; 
+  setBufferSize(uart_p, uart_p->buffer_size); // crea malloc
+  // si el usuario no lo ha hecho
+  uart_p->buffer_count = 0;
+  uint8_t * posicionInicial = uart_p->buffer_pointer_writing;
+
+
+  while(uart_p->thread_run) {
+    if(checkRxdata(uart_p)) {
+      *(uart_p->buffer_pointer_writing) = recibirChar(uart_p);
+      uart_p->buffer_pointer_writing++;
+    }
+  }
+
+  uart_p->buffer_count = 
+  (uint32_t)(uart_p->buffer_pointer_writing) - (uint32_t)posicionInicial;
+  return NULL;
+}
+
+void terminarRecepcion(UART_T * uart_p)
+{
+  uart_p->thread_run = false;
+}
+
+uint8_t * vaciarBuffer(UART_T * uart_p, uint32_t * num)
+{
+  *num = uart_p->buffer_count;
+  uint8_t * data_array = malloc(*num * sizeof(uint8_t));
+  uint8_t * data_array_init = data_array;
+
+  for(int i = 0; i<*num; i++) {
+    *data_array = *(uart_p->buffer_pointer_reading);
+    uart_p->buffer_pointer_reading++;
+    data_array++;
+  }
+  uart_p->buffer_pointer_reading = uart_p->buffer_pointer_init;
+  uart_p->buffer_pointer_writing = uart_p->buffer_pointer_init;
+  uart_p->buffer_count = 0;
+
+  return data_array_init;
 }
--- uart_core_lib.h
+++ uart_core_lib.h
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <pthread.h>
 #include <fcntl.h> // open
 
 typedef struct UART_S UART_T;
@@ -11,19 +12,40 @@ struct UART_S {
   uint32_t CONTROL;
   uint32_t DIVISOR;
   uint32_t ENDOFPACK;
+
+  // UART buffer for incoming data
+  uint32_t  buffer_size;
+  uint8_t * buffer_pointer_init;
+  uint8_t * buffer_pointer_writing;
+  uint8_t * buffer_pointer_reading;
+  uint32_t  buffer_count;
+  
+  pthread_t thread;
+  bool      thread_run;
 };
 
+// Initial config and utils
 UART_T uart_init(uint32_t baseadd);
-uint16_t UartRegister(uint32_t address);
+void setBaud(UART_T * uart_p, uint32_t clock_frequency, uint32_t baudrate);
+void setBufferSize(UART_T * uart_p, uint32_t size);
+void freeBuffer(UART_T * uart_p);
+
+// UART Core registers access
+uint16_t uartRegister(uint32_t address);
 void checkUartRegisters(UART_T * uart_p);
 void resetStatus(UART_T * uart_p);
-void setBaud(UART_T * uart_p, uint32_t clock_frequency, uint32_t baudrate);
 
 // proceso de envio de datos
-bool check_txdata(UART_T * uart_p);
-void enviarchar(UART_T * uart_p, char character);
-void enviarstring(UART_T * uart_p, char * str, int num);
+bool checkTxdata(UART_T * uart_p);
+void enviarChar(UART_T * uart_p, char character);
+void enviarString(UART_T * uart_p, char * str, int num);
 
 // proceso de recepcion de datos
-bool check_rxdata(UART_T * uart_p);
-char recibirchar(UART_T * uart_p);
+bool checkRxdata(UART_T * uart_p);
+uint8_t recibirChar(UART_T * uart_p);
+
+// proceso de recepcion automatica de datos
+void activarRecepcion(UART_T * uart_p);
+void * activarRecepcion_aux(void * ptr);
+void terminarRecepcion(UART_T * uart_p);
+uint8_t * vaciarBuffer(UART_T * uart_p, uint32_t * num);
